5f2311833060843141bc7416f8f798681fe5cceb
Abrahansito <huamancarlosabraham@gmail.com>
2025-12-15 23:36:12 -0500

    Esperar conexión MongoDB antes de levantar servidor

--- index.js
+++ index.js
@@ -12,19 +12,26 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// Ruta de prueba
+app.get("/", (req, res) => {
+  res.send("Backend del portafolio funcionando");
+});
 
 // Rutas
 app.use("/api/contact", contactRoutes);
 
 // Conexión a MongoDB
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB conectado"))
-.catch((err) => console.error("Error MongoDB:", err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB conectado");
 
-app.get("/", (req, res) => {
-  res.send("Backend del portafolio funcionando");
-});
+    app.listen(PORT, () => {
+      console.log(`Servidor escuchando en puerto ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error MongoDB:", error);
+  });
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+
