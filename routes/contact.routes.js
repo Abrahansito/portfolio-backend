8a67006f1c5cbf4d434762de16f8bdb8d142c876
Abrahansito <huamancarlosabraham@gmail.com>
2025-12-15 23:17:06 -0500

    Mostrar error real al guardar contacto

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -16,7 +16,9 @@ router.post("/", async (req, res) => {
 
     res.status(201).json({ message: "Mensaje enviado correctamente" });
   } catch (error) {
-    res.status(500).json({ error: "Error al guardar el mensaje" });
+    console.error("ERROR AL GUARDAR:", error);
+    res.status(500).json({ error: error.message });
+    /*res.status(500).json({ error: "Error al guardar el mensaje" });*/
   }
 });
 
